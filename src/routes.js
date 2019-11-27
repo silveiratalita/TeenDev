56501d66a2c0cf8931e7ecd3890c45788a7c77e5
joao <09356326975@edu.udesc.br>
2019-11-27 10:18:46 -0200

    Journey route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import PersonController from './app/controlllers/PersonController';
+import JourneyController from './app/controlllers/JourneyController';
 
 const routes = new Router();
+
 routes.post("/pessoas", PersonController.store);
+routes.post("/jornadas", JourneyController.store);
+
 export default routes;
